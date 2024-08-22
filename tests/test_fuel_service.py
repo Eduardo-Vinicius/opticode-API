02f6de8ea045c90827b0bba7618561fa67eeb050
unknown <eduardo.vinicius01@outlook.com>
2024-08-22 01:51:50 -0300

    alterado para pytest

--- tests/test_fuel_service.py
+++ tests/test_fuel_service.py
@@ -1,27 +1,22 @@
-import unittest
+import pytest
 from src.services.fuel_service import calcular_custo_combustivel
 
 
-class TestFuelService(unittest.TestCase):
-    def test_calcular_custo_combustivel(self):
-        # Dados de teste
-        distancia_total_km = 100  # 100 km
-        consumo_medio_km_por_litro = 10  # 10 km por litro
-        preco_combustivel_por_litro = 5.0  # R$ 5.00 por litro
+def test_calcular_custo_combustivel():
+    # Dados de teste
+    distancia_total_km = 100  # 100 km
+    consumo_medio_km_por_litro = 10  # 10 km por litro
+    preco_combustivel_por_litro = 5.0  # R$ 5.00 por litro
 
-        # Esperado: Custo total = (100 km / 10 km/litro) * R$ 5.00 = R$ 50.00
-        resultado_esperado = {
-            "distancia_total_km": 100,
-            "custo_total_combustivel": 50.0
-        }
+    # Esperado: Custo total = (100 km / 10 km/litro) * R$ 5.00 = R$ 50.00
+    resultado_esperado = {
+        "distancia_total_km": 100,
+        "custo_total_combustivel": 50.0
+    }
 
-        # Calcula o custo
-        resultado = calcular_custo_combustivel(distancia_total_km, consumo_medio_km_por_litro,
-                                               preco_combustivel_por_litro)
+    # Calcula o custo
+    resultado = calcular_custo_combustivel(distancia_total_km, consumo_medio_km_por_litro,
+                                           preco_combustivel_por_litro)
 
-        # Verifica se o resultado está correto
-        self.assertEqual(resultado, resultado_esperado)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # Verifica se o resultado está correto
+    assert resultado == resultado_esperado
